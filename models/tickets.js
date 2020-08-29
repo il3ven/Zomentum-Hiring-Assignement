c5792540df32e53034ae3384f40c18fca705c834
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-08-29 18:41:36 +0530

    Updated schema

--- models/tickets.js
+++ models/tickets.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const TicketSchema = new mongoose.Schema({
-  userid: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
   phoneNumber: Number,
   timing: Date, // Timing for movie show
 });
--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, required: true },
   firstName: String,
   lastName: String,
 });
