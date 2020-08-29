b2e041710d097006ba1bac17f3d0b6866e3d13ce
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-08-30 01:34:27 +0530

    Shifted Agenda to /lib

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Agenda = require("agenda");
 require("dotenv").config();
 const app = express();
 const PORT = 5000;
@@ -26,20 +25,12 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
-db.once("open", function () {
+db.once("open", async function () {
+  // EXPRESS
   app.listen(PORT, () => {
     console.log(`REST API is running at http://localhost:${PORT}`);
   });
-});
 
-// AGENDA JS
-const agenda = new Agenda({ mongo: db });
-agenda.define("test", async (job) => {
-  console.log("Agenda is running");
+  // AGENDA
+  require("./lib/agenda.js");
 });
-(async function () {
-  // IIFE to give access to async/await
-  await agenda.start();
-
-  await agenda.every("3 seconds", "test");
-})();
